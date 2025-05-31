39323115ac2b4945a553a5111291d05153fee848
Nakul Srivastava <imnakul44@gmail.com>
2025-05-31 21:39:43 +0530

    Conditional Statement

--- javascript/1.js
+++ javascript/1.js
@@ -9,3 +9,8 @@
 // hello()
 console.log('hello\r')
 console.log('Hello\rWorld') // Output: World
+
+// If else Conditional Statement
+let a = prompt('Entere a value')
+if (a % 5 === 0) console.log('its a multiple of 5')
+else console.log('Its not')
